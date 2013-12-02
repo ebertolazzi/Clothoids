2e256b66a8942b7c8ce1e85719ac0c629c3553bf
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2013-12-02 13:55:57 +0100

    Workaround for inclusion of pcre.h on windows

--- srcs/GenericContainer.hh
+++ srcs/GenericContainer.hh
@@ -400,7 +400,11 @@ in the distribution.
 
 // use pcre for pattern matching
 #ifndef GENERIC_CONTAINER_NO_PCRE
-#include <pcre.h>
+  #if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
+    #include <pcrecpp.h>
+  #else
+    #include <pcre.h>
+  #endif
 #endif
 
 // if C++ < C++11 define nullptr
