3948db61029aee677e1b32831b825638e09e1319
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2018-10-12 19:41:06 +0200

    Some fixes for Mingw and single test for polyline

--- tests-cpp/testPolyline.cc
+++ tests-cpp/testPolyline.cc
@@ -1,21 +1,32 @@
-#include <iostream>
-#include "Clothoid.hh"
+#define _USE_MATH_DEFINES
+
 #include "PolyLine.hh"
+#include "Clothoid.hh"
+#include <cmath>
+#include <iostream>
 
-int main(int argc, const char * argv[]) {
-  G2lib::ClothoidList curve;
-  G2lib::PolyLine poly;
-  int result = 14;
+using G2lib::real_type;
 
-  curve.push_back_G1(0.0, 0.0, 0.0, 0.0, 3.0, G2lib::m_pi);
-  std::cout << "Building polyline...";
-  poly.build(curve, 0.01);
-  std::cout << " done!" << std::endl;
-  std::cout << "Checking result: ";
-  if (poly.numSegment() ==result)
-    std::cout << "CORRECT" << std::endl;
-  else
-    std::cout << "WRONG" << std::endl;
-  
+int
+main() {
+
+  G2lib::PolyLine      P;
+  G2lib::ClothoidCurve C;
+
+  real_type x0  = 0;
+  real_type y0  = 0;
+  real_type th0 = 0;
+  real_type x1  = 0;
+  real_type y1  = 3;
+  real_type th1 = G2lib::m_pi;
+
+  C.build_G1( x0, y0, th0, x1, y1, th1 );
+  C.info(std::cout);
+
+  P.build( C, 0.001 );
+  P.info(std::cout);
+
+  std::cout << "\n\nALL DONE FOLKS!!!\n";
+
   return 0;
 }
