bd0bad3d39c3f75c01115fa354acc879245309fa
matteo.ragni <matteo.ragni@antemotion.com>
2021-10-19 16:57:31 +0200

    :snake: revision of BiarcList wrapper (and Biarc)
    Revision of BiarcList wrappers
    
     - removed new abstract methods included in BaseCurve
     - get methods as an alias: __getitem__
     - numSegment is now numSegments
     - findST1 has a different return value
     - documentation for all the methods
     - copy is now pythonic (returns the copy instead of applying on current object)

--- src_py/src/python-Biarc.cc
+++ src_py/src/python-Biarc.cc
@@ -53,11 +53,11 @@ namespace G2lib {
         py::arg("x1"), py::arg("y1"), py::arg("theta1"))
       .def(py::init<BaseCurve const &>(), py::arg("curve"))
       
-      .def("copy", [](const Biarc & self) {
+      .def("copy", [](const Biarc & self) -> Biarc {
         Biarc other;
         other.copy(self);
         return other;
-      }, py::arg("curve"),
+      },
       R"S(
         Create a copy of the current biarc curve
 
@@ -184,102 +184,322 @@ namespace G2lib {
     }
 
     void wrap_BiarcList(py::module & m) {
-      py::class_<BiarcList, BaseCurve>(m, "BiarcList")
-        .def(py::init<>())
-        .def(py::init<BaseCurve const &>())
-        .def(py::init<BiarcList const &>())
-        .def(py::init<PolyLine const &>())
-        .def(py::init<LineSegment const &>())
-        .def(py::init<CircleArc const &>())
-        .def(py::init<Biarc const &>())
-        .def("get", &BiarcList::get)
-        .def("copy", &BiarcList::copy)
-        .def("init", &BiarcList::init)
-        .def("reserve", &BiarcList::reserve)
-        .def("push_back", py::overload_cast<LineSegment const &>(&BiarcList::push_back))
-        .def("push_back", py::overload_cast<CircleArc const &>(&BiarcList::push_back))
-        .def("push_back", py::overload_cast<Biarc const &>(&BiarcList::push_back))
-        .def("push_back", py::overload_cast<PolyLine const &>(&BiarcList::push_back))
-        .def("push_back_G1", py::overload_cast<real_type, real_type, real_type>(&BiarcList::push_back_G1))
-        .def("push_back_G1", py::overload_cast<real_type, real_type, real_type, real_type, real_type, real_type>(&BiarcList::push_back_G1))
-        .def("build_G1", [](BiarcList * self, std::vector<real_type> x, std::vector<real_type> y) {
-          int_type n = static_cast<int_type>(std::min(x.size(), y.size()));
-          real_type * x_ = new real_type[n];
-          real_type * y_ = new real_type[n];
-          for (int_type i = 0; i < n; i++) {
-            x_[i] = x[i];
-            y_[i] = y[i];
-          }
-          bool ret = self->build_G1(n, x_, y_);
-          delete[] x_;
-          delete[] y_;
-          return ret;
-        })
-        .def("build_G1", [](BiarcList * self, std::vector<real_type> x, std::vector<real_type> y, std::vector<real_type> theta) {
-          int_type n = static_cast<int_type>(std::min({x.size(), y.size(), theta.size()}));
-          real_type * x_ = new real_type[n];
-          real_type * y_ = new real_type[n];
-          real_type * theta_ = new real_type[n];
-          for (int_type i = 0; i < n; i++) {
-            x_[i] = x[i];
-            y_[i] = y[i];
-            theta_[i] = theta[i];
-          }
-          bool ret = self->build_G1(n, x_, y_);
-          delete[] x_;
-          delete[] y_;
-          delete[] theta_;
-          return ret;
-        })
-        .def("getAtS", &BiarcList::getAtS)
-        .def("numSegments", &BiarcList::numSegments)
-        .def("findAtS", &BiarcList::findAtS)
-        .def("segment_length", &BiarcList::segment_length)
-        .def("segment_length_ISO", &BiarcList::segment_length_ISO)
-        .def("build_AABBtree_ISO", &BiarcList::build_AABBtree_ISO)
-        .def("build_AABBtree_SAE", &BiarcList::build_AABBtree_SAE, 
-          py::arg("offs"), py::arg("max_angle") = Utils::m_pi/6, py::arg("max_size") = 1e100)
-        .def("getSTK", [](BiarcList * self) {
-          int_type n = self->numSegments();
-          std::vector<real_type> s, t, k;
-          real_type * s_ = new real_type[n];
-          real_type * t_ = new real_type[n];
-          real_type * k_ = new real_type[n];
-          self->getSTK(s_, t_, k_);
-          for (int_type i = 0; i < n; i++) {
-            s.push_back(s_[i]);
-            t.push_back(t_[i]);
-            k.push_back(k_[i]);
-          }
-          delete[] s_;
-          delete[] t_;
-          delete[] k_;
-          return std::make_tuple(s, t, k);
-        })
-        .def("getXY", [](BiarcList * self) {
-          int_type n = self->numSegments();
-          std::vector<real_type> x, y;
-          real_type * x_ = new real_type[n];
-          real_type * y_ = new real_type[n];
-          self->getXY(x_, y_);
-          for (int_type i = 0; i < n; i++) {
-            x.push_back(x_[i]);
-            y.push_back(y_[i]);
-          }
-          delete[] x_;
-          delete[] y_;
-          return std::make_tuple(x, y);
-        })
-        .def("findST1", [](BiarcList * self, real_type x, real_type y) {
-          real_type s, t;
-          self->findST1(x, y, s, t);
-          return std::make_tuple(s, t);
-        })
-        .def("findST1", [](BiarcList * self, int_type ibegin, int_type iend, real_type x, real_type y) {
-          real_type s, t;
-          self->findST1(ibegin, iend, x, y, s, t);
-          return std::make_tuple(s, t);
-        });
+      py::class_<BiarcList, BaseCurve>(m, "BiarcList",
+      R"S(
+        A class to manage a list of biarc curve (not necessarily G1 or G2 connected)
+
+        There exists several constructors for this class:
+         
+         * empty constructor
+         * constructor receiving a curve as parameter (BaseCurve or BiarcList or PolyLine
+           or LineSegment or CircleArc or Biarc)
+      )S")
+
+      .def(py::init<>())
+      .def(py::init<BaseCurve const &>())
+      .def(py::init<BiarcList const &>())
+      .def(py::init<PolyLine const &>())
+      .def(py::init<LineSegment const &>())
+      .def(py::init<CircleArc const &>())
+      .def(py::init<Biarc const &>())
+      
+      .def("get", &BiarcList::get, py::arg("idx"),
+      R"S(
+        Returns the `idx`-th element of the list
+
+        :param int idx: index
+        :return: the biarc at that index
+        :rtype: Biarc
+      )S")
+
+      .def("__getitem__", &BiarcList::get, py::arg("idx"),
+      R"S(
+        Returns the `idx`-th element of the list
+      
+        :param int idx: index
+        :return: the biarc at that index
+        :rtype: Biarc
+      )S")
+
+      .def("copy", [](const BiarcList & self) {
+        BiarcList other;
+        other.copy(self);
+        return other;
+      }, 
+      R"S(
+        Returns a copy of the current biarc list
+
+        :return: a copy of the current biarc list
+        :rtype: BiarcList
+      )S")
+      
+      .def("init", &BiarcList::init,
+      R"S(
+        Empties the current biarc list
+
+        :return: nothing, works in place
+        :rtype: NoneType
+      )S")
+
+      .def("reserve", &BiarcList::reserve, py::arg("n"),
+      R"S(
+        Reserve memory for **n** biarc in the list
+
+        :return: nothing, works in place
+        :rtype: NoneType
+      )S")
+
+      .def("push_back", py::overload_cast<LineSegment const &>(&BiarcList::push_back), py::arg("c"),
+      R"S(
+        Append a line segment to the current biarc list. The curve is transformed
+        to a degenerate biarc curve
+
+        :param LineSegment c: the line segment
+        :return: nothing, works in place
+        :rtype: NoneType
+      )S")
+
+      .def("push_back", py::overload_cast<CircleArc const &>(&BiarcList::push_back), py::arg("c"),
+      R"S(
+        Append a circle arc to the current biarc list. The curve is transformed
+        to a degenerate biarc curve
+
+        :param CircleArc c: the circle arc
+        :return: nothing, works in place
+        :rtype: NoneType
+      )S")
+
+      .def("push_back", py::overload_cast<Biarc const &>(&BiarcList::push_back), py::arg("c"),
+      R"S(
+        Append a biarc to the current biarc list.
+
+        :param BiarcList c: the biarc
+        :return: nothing, works in place
+        :rtype: NoneType
+      )S")
+
+      .def("push_back", py::overload_cast<PolyLine const &>(&BiarcList::push_back), py::arg("c"),
+      R"S(
+        Append a polyline to the current biarc list. The curve is transformed
+        to a degenerate biarc curve
+
+        :param PolyLine c: the polyline
+        :return: nothing, works in place
+        :rtype: NoneType
+      )S")
+
+      .def("push_back_G1", py::overload_cast<real_type, real_type, real_type>(&BiarcList::push_back_G1),
+      py::arg("x1"), py::arg("y1"), py::arg("theta1"),
+      R"S(
+        Construct a biarc passing from the points
+        :math:`(x_0,y_0)` to the point  :math:`(x_1,y_1)`
+        with initial angle :math:`\theta_0` and final angle :math:`\theta_1`
+        and append the biarc to the tail of biarc list.
+        The initial point and angle is taken from the tail of the biarc list.
+        
+        :param float x1:     :math:`x_1`
+        :param float y1:     :math:`y_1`
+        :param float theta1: :math:`\theta_1`
+        :return: nothing, works in place
+        :rtype: NoneType
+      )S")
+
+      .def("push_back_G1", py::overload_cast<real_type, real_type, real_type, real_type, real_type, real_type>(&BiarcList::push_back_G1),
+      py::arg("x0"), py::arg("y0"), py::arg("theta0"), py::arg("x1"), py::arg("y1"), py::arg("theta1"),
+      R"S(
+        Construct a biarc passing from the points
+        :math:`(x_0,y_0)` to the point  :math:`(x_1,y_1)`
+        with initial angle :math:`\theta_0` and final angle :math:`\theta_1`
+        and append the biarc to the tail of biarc list.
+        
+        :param float x0:     :math:`x_0`
+        :param float y0:     :math:`y_0`
+        :param float theta0: :math:`\theta_0`
+        :param float x1:     :math:`x_1`
+        :param float y1:     :math:`y_1`
+        :param float theta1: :math:`\theta_1`
+        :return: nothing, works in place
+        :rtype: NoneType
+      )S")
+      
+      .def("build_G1", [](BiarcList & self, std::vector<real_type> x, std::vector<real_type> y) {
+        const size_t n = std::min(x.size(), y.size());
+        return self.build_G1(n, x.data(), y.data());
+      }, py::arg("x"), py::arg("y"),
+      R"S(
+        Constructs a G1 biarc list using the points provided. The two vectors 
+        are trimmed to the length of the shortest one.
+
+        :param List[float] x: list of x coordinates
+        :param List[float] y: list of y coordinates
+        :return: true if build succeeds
+        :rtype: bool
+      )S")
+
+      .def("build_G1", [](BiarcList & self, const std::vector<real_type> & x, 
+        const std::vector<real_type> & y, const std::vector<real_type> & theta) {
+        const size_t n = std::min({x.size(), y.size(), theta.size()});
+        return self.build_G1(n, x.data(), y.data(), theta.data());
+      }, py::arg("x"), py::arg("y"), py::arg("theta"),
+      R"S(
+        Constructs a G1 biarc list using the points provided. The two vectors 
+        are trimmed to the length of the shortest one. This version allows to 
+        specify the angle at nodes.
+
+        :param List[float] x: list of x coordinates
+        :param List[float] y: list of y coordinates
+        :param List[float] theta: angles at nodes
+        :return: true if build succeeds
+        :rtype: bool
+      )S")
+
+      .def("getAtS", &BiarcList::getAtS, py::arg("s"),
+      R"S(
+        Get the biarc at coordinates **s**
+
+        :param float s: **s** coordinates
+        :return: biarc at **s**
+        :rtype: Biarc
+      )S")
+      
+      .def("numSegments", &BiarcList::numSegments,
+      R"S(
+        Returns the number of elements in the BiarcList
+
+        :return: the number of elements
+        :rtype: int
+      )S")
+
+      .def("__len__", &BiarcList::numSegments,
+      R"S(
+        Returns the number of elements in the BiarcList
+
+        :return: the number of elements
+        :rtype: int
+      )S")
+      
+      .def("findAtS", &BiarcList::findAtS, py::arg("s"),
+      R"S(
+        Get the index of the biarc at coordinate **s**
+
+        :param float s: coordinate **s**
+        :return: index of the biarc at that coordinate
+        :rtype: int
+      )S")
+      
+      .def("segment_length", &BiarcList::segment_length, py::arg("nseg"),
+      R"S(
+        Return the length of the segment at index `nseg`
+
+        :param int nseg: index of the segment
+        :return: length of the segment at that index
+        :rtype: float
+      )S")
+      
+      .def("segment_length_ISO", &BiarcList::segment_length_ISO, py::arg("nseg"), py::arg("offs"),
+      R"S(
+        Return the length of the segment at index `nseg`. Version with ISO offset.
+
+        :param int nseg: index of the segment
+        :param float offs: offset from the curve
+        :return: length of the segment at that index
+        :rtype: float
+      )S")
+      
+      .def("build_AABBtree_ISO", &BiarcList::build_AABBtree_ISO,
+        py::arg("offs"), py::arg("max_angle") = Utils::m_pi/6, py::arg("max_size") = 1e100,
+      R"S(
+        Build the internal AABB tree of the biarc list with offset (ISO)
+        
+        :param float offs:      curve offset
+        :param float max_angle: maximum angle variation of the arc covered by a triangle
+        :param float max_size:  maximum admissible size of the covering tirnagles
+        :return: nothing, works in place
+        :rtype: NoneType
+      )S")
+      
+      .def("build_AABBtree_SAE", &BiarcList::build_AABBtree_SAE,
+        py::arg("offs"), py::arg("max_angle") = Utils::m_pi/6, py::arg("max_size") = 1e100,
+      R"S(
+        Build the internal AABB tree of the biarc list with offset (SAE)
+        
+        :param float offs:      curve offset
+        :param float max_angle: maximum angle variation of the arc covered by a triangle
+        :param float max_size:  maximum admissible size of the covering tirnagles
+        :return: nothing, works in place
+        :rtype: NoneType
+      )S")
+      
+      .def("getSTK", [](const BiarcList & self) {
+        const size_t n = self.numSegments();
+        // Avoids segmentation fault for empty list
+        if (!n) {
+          return make_tuple(std::vector<real_type>(), 
+                            std::vector<real_type>(), 
+                            std::vector<real_type>());
+        }
+        std::vector<real_type> s(n), t(n), k(n);
+        self.getSTK(s.data(), t.data(), k.data());
+        return std::make_tuple(s, t, k);
+      },
+      R"S(
+        Returns the list of nodes angles and curvatures of the biarc
+
+        :return: s, angles and curvatures in nodes as lists
+        :rtype: Tuple[List[float], List[float], List[float]]
+      )S")
+      
+      .def("getXY", [](const BiarcList & self) {
+        const size_t n = self.numSegments();
+        // Avoids segmentation fault for empty list
+        if (!n) {
+          return make_tuple(std::vector<real_type>(), 
+                            std::vector<real_type>());
+        }
+        std::vector<real_type> x(n), y(n);
+        self.getXY(x.data(), y.data());
+        return std::make_tuple(x, y);
+      },
+      R"S(
+        Returns the list of nodes coordinates
+
+        :return: nodes coordinates, a list of x and y values
+        :rtype: Tuple[List[float], List[float]]
+      )S")
+      
+      .def("findST1", [](const BiarcList & self, real_type x, real_type y) {
+        real_type s, t;
+        int_type idx = self.findST1(x, y, s, t);
+        return std::make_tuple(idx, s, t);
+      }, py::arg("x"), py::arg("y"),
+      R"S(
+        Find parametric coordinate :math:`(s, t)` given a point. With respect to the 
+        classic `findST`, this version returns as first return value the index
+        of the segment.
+
+        :param float x: **x** coordinates of the point
+        :param float y: **y** coordinates of the point
+        :return: a tuple with index, **s** coordinates and **t** coordinates
+        :rtype: Tuple[int, float, float]
+      )S")
+      
+      .def("findST1", [](const BiarcList & self, int_type ibegin, int_type iend, real_type x, real_type y) {
+        real_type s, t;
+        int_type idx = self.findST1(ibegin, iend, x, y, s, t);
+        return std::make_tuple(idx, s, t);
+      }, py::arg("ibegin"), py::arg("iend"), py::arg("x"), py::arg("y"),
+      R"S(
+        Find parametric coordinate :math:`(s, t)` given a point. With respect to the 
+        classic `findST`, this version returns as first return value the index
+        of the segment. The search is done only between elements ``ibegin..iend``.
+
+        :param int ibegin: initial index
+        :param int iend: final index
+        :param float x: **x** coordinates of the point
+        :param float y: **y** coordinates of the point
+        :return: a tuple with index, **s** coordinates and **t** coordinates
+        :rtype: Tuple[int, float, float]
+      )S");
     }
   }
 }
