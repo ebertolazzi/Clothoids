e175793004b1816fec6ac7ad1e24a96e22896110
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2022-11-02 11:39:32 +0100

    added sub_branches.yaml for setup.rb

--- setup.rb
+++ setup.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 puts "Setup submodules"
 system('git submodule init')
 system('git submodule update')
@@ -5,6 +7,17 @@ system('git submodule sync')
 system('git submodule foreach --recursive git submodule init')
 system('git submodule foreach --recursive git submodule update')
 system('git submodule foreach --recursive git submodule sync')
+
+branches = YAML.load_file("./sub_branches.yaml")
+branches.each do |dir, branch|
+  puts "\n\nChecking out branch #{branch} in #{dir}"
+  if branch.include? ":" then
+    res = branch.split(':');
+    system("(cd #{dir} && git reset --hard && git fetch --tags && git checkout '#{res[1]}' )")
+  else
+    system("(cd #{dir} && git reset --hard && git checkout #{branch} && git pull && git reset --hard)")
+  end
+end
 
 puts ARGV
 
